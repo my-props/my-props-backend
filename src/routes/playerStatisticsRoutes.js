b2d0612b9fb46b79157e01079a8dd71383c788ef
Bruno Wendt <brunocwendt@gmail.com>
2024-05-03 15:11:37 -0300

    player vs team stats

--- src/routes/playerStatisticsRoutes.js
+++ src/routes/playerStatisticsRoutes.js
@@ -13,7 +13,7 @@ router.get('/matchup-statistics/:teamId1/:teamId2', async (req, res) => {
   const query = `
   WITH relevant_games AS (
     SELECT
-        game_id,
+        id,
         home_id,
         visitor_id,
         home_total_points,
@@ -27,7 +27,7 @@ router.get('/matchup-statistics/:teamId1/:teamId2', async (req, res) => {
 )
 SELECT
     ps.team_id,
-    COUNT(DISTINCT rg.game_id) AS num_games,
+    COUNT(DISTINCT rg.id) AS num_games,
     AVG(CASE
         WHEN g.home_id = ps.team_id THEN g.home_total_points
         ELSE g.visitor_total_points
@@ -48,9 +48,9 @@ SELECT
 FROM
     player_statistics ps
 JOIN
-    relevant_games rg ON ps.game_id = rg.game_id
+    relevant_games rg ON ps.game_id = rg.id
 JOIN
-    games g ON g.game_id = rg.game_id
+    games g ON g.id = rg.id
 WHERE
     ps.team_id IN ($2, $1)
 GROUP BY
@@ -68,5 +68,68 @@ GROUP BY
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+router.get('/player-match-statistics/:playerId/:teamId', async (req, res) => {
+  const playerId = parseInt(req.params.playerId);
+  const teamId = parseInt(req.params.teamId);
+
+  if (!playerId || !teamId) {
+    return res.status(400).json({ error: 'Invalid team IDs provided' });
+  }
+
+  const query = `
+  WITH relevant_games AS (
+    SELECT *
+    FROM games
+    WHERE (
+        (home_id = (SELECT team_id FROM players WHERE id = $1) AND visitor_id = $2) OR
+        (home_id = $2 AND visitor_id = (SELECT team_id FROM players WHERE id = $1))
+    )
+    AND date_start >= '2023-10-24'::date AND date_start <= CURRENT_DATE
+),
+player_team_id AS (
+    SELECT team_id
+    FROM players
+    WHERE id = $1
+)
+
+SELECT ROUND(AVG(points), 2) AS avg_10_points,
+        ROUND(AVG(field_goal_made), 2) AS avg_10_field_goal_made,
+        ROUND(AVG(field_goal_attempt), 2) AS avg_10_field_goal_attempt,
+        ROUND(AVG(three_points_made), 2) AS avg_10_three_points_made,
+        ROUND(AVG(three_points_attempt), 2) AS avg_10_three_points_attempt,
+        ROUND(AVG(offensive_rebound), 2) AS avg_10_offensive_rebound,
+        ROUND(AVG(defensive_rebound), 2) AS avg_10_defensive_rebound,
+        ROUND(AVG(total_rebound), 2) AS avg_10_total_rebound,
+        ROUND(AVG(assists), 2) AS avg_10_assists,
+        ROUND(AVG(personal_fouls), 2) AS avg_10_personal_fouls,
+        ROUND(AVG(steals), 2) AS avg_10_steals,
+        ROUND(AVG(turnovers), 2) AS avg_10_turnovers,
+        ROUND(AVG(blocks), 2) AS avg_10_blocks
+FROM 
+    player_statistics ps
+JOIN 
+    relevant_games rg ON ps.game_id = rg.id
+JOIN
+    player_team_id pti ON ps.team_id = pti.team_id
+WHERE 
+    ps.player_id = $1
+    group by ps.player_id;
+  `;
+
+  try {
+    const { rows } = await db.query(query, [playerId, teamId]);
+    if (rows.length === 0) {
+      return res.status(404).json({ error: 'No matchup data found for the provided team IDs' });
+    }
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('Error querying matchup statistics:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
 
 module.exports = router;
