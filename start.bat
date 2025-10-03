c125747f03cbfeda5c3e7af7d43a2c7760ad20b7
Bruno Cwendt <brunocwendt@gmail.com>
2025-10-03 10:55:41 -0300

    delete wrong file

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting My Props Backend...
-set DB_HOST=az-my-props.database.windows.net
-set DB_NAME=hma-my-props
-set DB_USER=mypropsadmin
-set DB_PASS=MyPr0psAdm
-set DB_PORT=5432
-set PORT=3000
-node src/app.js
